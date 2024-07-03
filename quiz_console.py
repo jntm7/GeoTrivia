8f83793c95b759abb130a6dfe5bbc1ed6450ff07
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-07-03 16:14:24 -0600

    Create quiz_console.py

---
+++ quiz_console.py
@@ -0,0 +1,48 @@
+import random
+from utils.questions import get_questions
+
+def run_quiz_console():
+    score = 0
+    category_id = 22
+    questions = get_questions(category_id)
+
+    for question_number, question in enumerate(questions, start=1):
+        print("\n" + "-" * 40)
+        print(f"\nQuestion {question_number}:")
+        print("\n" + question['question'])
+        answers = question['incorrect_answers'] + [question['correct_answer']]
+        random.shuffle(answers)
+        print()
+        for i, answer in enumerate(answers):
+            print(f"{chr(65+i)}. {answer}")
+        print()
+        user_answer = input("Please select your answer: ").upper()
+        if answers[ord(user_answer) - 65] == question['correct_answer']:
+            print("\nCorrect! Well done!")
+            score += 1
+        else:
+            print()
+            print(f"Incorrect!\nThe correct answer was: [{chr(65 + i)}] {question['correct_answer']}")
+
+    print("\n" + "-" * 40)
+    print(f"Final Score: {score} / {len(questions)}")
+    print()
+    final_score_message(score, len(questions))
+    print()
+    print("Thanks for playing GeoTrivia!")
+    print("=" * 40)
+
+def final_score_message(score, total):
+    if score == total:
+        print("You got a perfect score, amazing job!")
+    elif score >= total * 0.8:
+        print("Excellent work, you did very good!")
+    elif score >= total * 0.6:
+        print("Good job, your parents are proud!")
+    elif score >= total * 0.4:
+        print("Not bad, but I know you can do better!")
+    else:
+        print("Good effort, better luck next time!")
+
+if __name__ == "__main__":
+    run_quiz_console()
