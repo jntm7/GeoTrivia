eba983173fb20ae8f28b499385ba7aebc741b0d5
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-07-03 20:17:42 -0600

    Update to handle html decoding

--- utils/questions.py
+++ utils/questions.py
@@ -1,7 +1,13 @@
 import requests
+import html
 
 def get_questions(category_id = 22, amount = 10):
         url = f"https://opentdb.com/api.php?amount={amount}&category={category_id}&type=multiple"
         response = requests.get(url)
         data = response.json()
-        return data['results']
+        questions = data['results']
+        for question in questions:
+                question['question'] = html.unescape(question['question'])
+                question['correct_answer'] = html.unescape(question['correct_answer'])
+                question['incorrect_answers'] = [html.unescape(ans) for ans in question['incorrect_answers']]
+        return questions
