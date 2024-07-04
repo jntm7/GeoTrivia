52f3a3b4908290f514d578d06e103b4b4517fbe4
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-07-03 20:09:09 -0600

    Update quiz_console.py

--- quiz_console.py
+++ quiz_console.py
@@ -12,7 +12,7 @@ def run_quiz_console():
     questions = get_questions(category_id)
 
     for question_number, question in enumerate(questions, start=1):
-        print("\n" + "-" * 40)
+        print("\n" + "-" * 50)
         print(f"\nQuestion {question_number}:")
         print("\n" + question['question'])
         answers = question['incorrect_answers'] + [question['correct_answer']]
@@ -22,28 +22,30 @@ def run_quiz_console():
             print(f"{chr(65+i)}. {answer}")
         print()
 
+        correct_answer_index = answers.index(question['correct_answer'])
+
         while True:
             user_answer = input("Please select your answer: ").upper()
             if user_answer in ['A', 'B', 'C', 'D'] and len(user_answer) == 1:
                 answer_index = ord(user_answer) - 65
                 if answer_index < len(answers):
                     if answers[answer_index] == question['correct_answer']:
-                        print("\033[92m\nCorrect! Well done!\033[0m")
+                        print(green_text("Correct! Well done!"))
                         score += 1
                     else:
                         print()
-                        print(f"\033[91mIncorrect!\nThe correct answer was: [{chr(65 + i)}] {question['correct_answer']}\033[0m")
+                        print(red_text(f"Incorrect!\nThe correct answer was: [{chr(65 + correct_answer_index)}] {question['correct_answer']}"))
                 break
             else:
                 print("\nPlease enter a valid option (A, B, C, D):")
 
-    print("\n" + "-" * 40)
+    print("\n" + "-" * 50)
     print(f"Final Score: {score} / {len(questions)}")
     print()
     final_score_message(score, len(questions))
     print()
     print("Thanks for playing GeoTrivia!")
-    print("=" * 40)
+    print("=" * 50)
 
 def final_score_message(score, total):
     if score == total:
@@ -54,8 +56,10 @@ def final_score_message(score, total):
         print("Good job, your parents are proud!")
     elif score >= total * 0.4:
         print("Not bad, but I know you can do better!")
-    else:
+    elif score >= total * 0.2:
         print("Good effort, better luck next time!")
+    else:
+        print("Nice try, but you need to study up!")
 
 if __name__ == "__main__":
     run_quiz_console()
